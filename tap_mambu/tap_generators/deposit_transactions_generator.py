6151e06c11504e6dc55952ae02160dc4f2569c1b
jacobtuckett <jacob.tuckett@kroo.com>
2024-02-09 16:23:17 +0000

    convert dt to str in correct place

--- tap_mambu/tap_generators/deposit_transactions_generator.py
+++ tap_mambu/tap_generators/deposit_transactions_generator.py
@@ -13,15 +13,15 @@ class DepositTransactionsGenerator(MultithreadedBookmarkGenerator):
         self.endpoint_path = "deposits/transactions:search"
         self.endpoint_bookmark_field = "creationDate"
         self.endpoint_sorting_criteria["field"] = "id"
-        self.dt_start_date = datetime_to_utc_str(str_to_localized_datetime(
-                    get_bookmark(self.state, self.stream_name, self.sub_type, self.start_date)))
+        self.dt_start_date = str_to_localized_datetime(
+                    get_bookmark(self.state, self.stream_name, self.sub_type, self.start_date))
         self.dt_end_date = add_days(self.dt_start_date, self.days_to_process)
         self.endpoint_filter_criteria = [
             {
                 "field": "creationDate",
                 "operator": "BETWEEN",
-                "value": self.dt_start_date,
-                "secondValue" : self.dt_end_date
+                "value": datetime_to_utc_str(self.dt_start_date),
+                "secondValue" : datetime_to_utc_str(self.dt_end_date)
             }
         ]
 
